eb80ece24e644dc1fdebd66ad8d41c9bb69f6052
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-10-18 18:54:36 -0300

    Update Especialista.hpp

--- modulo5/vpl2/Especialista.hpp
+++ modulo5/vpl2/Especialista.hpp
@@ -7,18 +7,18 @@
 using namespace std;
 
 class Especialista:public Funcionario{
-  public:
-    string especialidade;
+    public:
+        string especialidade;
   
     double comissao(double ValorVenda){
-      double comissaoTotal = ValorVenda*(0.1);
-      return comissaoTotal;
+        double comissaoTotal = ValorVenda*(0.1);
+        return comissaoTotal;
     }
 
     void print(){
-      cout << "[Especialista]" << endl;
-      Funcionario::print();
-      cout << "Nome: " << nome << endl << "SalarioBase: R$ " << fixed << setprecision(2) << salarioBase <<endl;
+        cout << "[Especialista]" << endl;
+        Funcionario::print();
+        cout << "Nome: " << nome << endl << "SalarioBase: R$ " << fixed << setprecision(2) << salarioBase <<endl;
     }
 };
 
