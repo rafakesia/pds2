160609835f9ce9a123f6378bf232bba32fad88ea
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-10-18 19:03:35 -0300

    Update fila.cpp

--- modulo6/vpl1/fila.cpp
+++ modulo6/vpl1/fila.cpp
@@ -1,32 +1,32 @@
 #include "fila.h"
 
 struct No {
-  int chave;
-  No* proximo;
+    int chave;
+    No* proximo;
 };
 
 Fila::Fila() {
 }
 
 void Fila::Inserir(int k) {
-  myFila.push_back(k);
+    myFila.push_back(k);
 }
 
 void Fila::RemoverPrimeiro() {
-  if(myFila.empty()) throw ExcecaoFilaVazia();
-  myFila.pop_front();
+    if(myFila.empty()) throw ExcecaoFilaVazia();
+    myFila.pop_front();
 }
 
 int Fila::primeiro() const {
-  if(myFila.empty()) throw ExcecaoFilaVazia();
-  return myFila.front();
+    if(myFila.empty()) throw ExcecaoFilaVazia();
+    return myFila.front();
 }
 
 int Fila::ultimo() const {
-  if(myFila.empty()) throw ExcecaoFilaVazia();
-  return myFila.back();
+    if(myFila.empty()) throw ExcecaoFilaVazia();
+    return myFila.back();
 }
 
 int Fila::tamanho() const {
-  return myFila.size();
+    return myFila.size();
 }
