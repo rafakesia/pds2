14e5cd4619bf4b2aa1d3d9fa954f0adffce7e1a1
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-10-18 18:51:33 -0300

    Update Nave.hpp

--- modulo1/vpl1/Nave.hpp
+++ modulo1/vpl1/Nave.hpp
@@ -3,7 +3,7 @@
 #include "Ponto2D.hpp"
 
 struct Nave{
-	//Dados
+    //Dados
     Ponto2D posicao;
     double forca;
     double energia=100;
@@ -12,12 +12,12 @@ struct Nave{
     Nave(Ponto2D _posicao, double _forca);
     Nave() = default;
 
-	//Operacoes
-	void mover(double dx, double dy);
-	double calcular_distancia(Nave* nave);
-	Nave* determinar_nave_mais_proxima(Nave** naves, int n);
-	void atacar(Nave** naves, int n);
-	void imprimir_status();
+    //Operacoes
+    void mover(double dx, double dy);
+    double calcular_distancia(Nave* nave);
+    Nave* determinar_nave_mais_proxima(Nave** naves, int n);
+    void atacar(Nave** naves, int n);
+    void imprimir_status();
 };
 
 #endif
